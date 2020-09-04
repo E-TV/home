8f81c6f8a3218b72d5a57f4a86a1e994d76c13f2
E-TV <60294392+E-TV@users.noreply.github.com>
2020-09-04 11:16:35 +0200

    Add files via upload

---
+++ ckeditor.php
@@ -0,0 +1,152 @@
+﻿<!DOCTYPE HTML>
+<!--
+	Editorial by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>12V Portable Powerbank</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
+
+							<!-- Header -->
+								<header id="header">
+									  <p href="index.html" style="color:black;font-size:30px;">CKEditor</p>
+									<ul class="icons">
+										<li><a href="https://github.com/E-TV" class="icon brands fa-github"><span class="label">YouTube</span></a></li>
+										<li><a href="https://www.youtube.com/channel/UCVW9AqTwOfjLOtqSVVc8O8A" class="icon brands fa-youtube"><span class="label">Github</span></a></li>
+
+										
+										
+										<!--<li><a href="#" class="icon brands info-circle"><span class="label">Instructables</span></a></li>
+										<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+										<li><a href="#" class="icon brands fa-medium-m"><span class="label">Medium</span></a></li>-->
+									</ul>
+								</header>
+
+								
+								<script language="Javascript">
+		                      	<!--hide
+
+		                    	var password;
+			                    var pass="editit";
+		                     	password=prompt('Enter you Password here','');
+
+		                     	if(password==pass)
+		                       	alert('Correct Password, Click OK to Enter Website');
+			                    else
+			                    {
+			                    alert('Wrong Password, you will be returned to the Homepage!');
+			                  	window.location="https://e-tv.github.io/";
+			                    }
+			                    //-->
+			                    </script>
+
+
+							<textarea name "content"></textarea>
+								
+
+								
+							
+
+							
+
+
+								
+
+						</div>
+					</div>
+
+				<!-- Sidebar -->
+					<div id="sidebar">
+						<div class="inner">
+
+							
+
+							<!-- Menu -->
+								<nav id="menu">
+									<header class="major">
+										<h2>Menu</h2>
+									</header>
+									<ul>
+										<li><a href="index.html">Homepage</a></li>
+										<!--<li><a href="generic.html">Generic</a></li>
+										<li><a href="elements.html">Elements</a></li>-->
+										<li>
+											<span class="opener">Projects</span>
+											<ul>
+												<li><a href="powerbank.html">12V Portable Powerbank</a></li>
+												<li><a href="https://www.instructables.com/id/BT-Box/">BT-Box</a></li>
+												<li><a href="https://www.instructables.com/id/How-to-Make-Your-Own-PCBs/">How to Make Your Own PCB's</a></li>
+												
+
+
+
+
+
+												<!--<li><a href="#">Feugiat Veroeros</a></li>-->
+											</ul>
+
+											<span class="opener">Music</span>
+											<ul>
+												<li><a href="https://www.youtube.com/watch?v=sTsVJ1PsnMs">Vamos a la Playa</a></li>
+												<li><a href="https://www.instructables.com/id/BT-Box/">BT-Box</a></li>
+												<li><a href="https://www.instructables.com/id/How-to-Make-Your-Own-PCBs/">How to Make Your Own PCB's</a></li>
+												<li><a href="https://www.instructables.com/id/How-to-Make-Your-Own-PCBs/">Music</a></li>
+
+
+
+
+
+												<!--<li><a href="#">Feugiat Veroeros</a></li>-->
+											</ul>
+										</li>
+										<li><a href="contact.html">Contact</a></li>
+										
+										
+									</ul>
+								</nav>
+
+							
+
+							
+								
+
+								
+						
+								
+							</section>
+							
+							<!-- Footer -->
+								<footer id="footer">
+									<p class="copyright">&copy; Untitled. All rights reserved. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
+								</footer>
+
+						</div>
+					</div>
+
+			</div>
+
+		
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+			<script src="ckeditor/ckeditor.js"></script>
+			<script>
+			CKEDITOR.replace("content");
+			</script>
+	</body>
+</html>
