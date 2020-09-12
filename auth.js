b9619b554d2a57676bbe92b8158d9e65d51a106f
E-TV <60294392+E-TV@users.noreply.github.com>
2020-09-12 16:17:15 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -17,7 +17,7 @@ const uiConfig = {
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     firebase.auth.GithubAuthProvider.PROVIDER_ID,
-    firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+    
     
     // Additional login options should be listed here
     // once they are enabled within the console.
